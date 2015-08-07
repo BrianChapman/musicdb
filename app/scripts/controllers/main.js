592227e1604c1ff3b1ee4d00bfdfeee048db0136
Brian Chapman <brian@700c.net>
2015-08-07 14:09:00 -0700

    Initial commit of angular project generated by yeoman angular generator   - added angular material stylesheets to project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('musicdbApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
